08e024132f1a143e65fab88f7c906fb1d28ab545
sinarab <sinara_b@outlook.com>
2024-08-18 23:13:09 -0300

    lesson 16 - adding metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
 export default function Page() {
-    return (
-        <div>Costumers</div>
-    );
+  return <div>Costumers</div>;
 }
